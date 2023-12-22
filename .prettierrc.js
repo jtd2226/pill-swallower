a3fbc936825409a811f89a551d2251f99c0aea12
Joel Davis <joel_davis2@apple.com>
2023-12-22 17:07:12 -0600

    initial project setup

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'none',
+  tabWidth: 2,
+  useTabs: false,
+  arrowParens: 'avoid',
+  printWidth: 180,
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,131 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client';
+
+import { useEffect } from 'react';
+
+function onload() {
+  const controls = document.querySelector('.controls');
+  const cameraOptions = document.querySelector('.video-options>select') as HTMLSelectElement;
+  const video = document.querySelector('video') as HTMLVideoElement;
+  const canvas = document.querySelector('canvas') as HTMLCanvasElement;
+  const screenshotImage = document.querySelector('img') as HTMLImageElement;
+  const buttons = Array.from(controls!.querySelectorAll('button'));
+  let streamStarted = false;
+
+  const [play, pause, screenshot] = buttons;
+
+  const constraints = {
+    video: {
+      width: {
+        min: 1280,
+        ideal: 1920,
+        max: 2560
+      },
+      height: {
+        min: 720,
+        ideal: 1080,
+        max: 1440
+      }
+    }
+  };
+
+  cameraOptions.onchange = () => {
+    const updatedConstraints = {
+      ...constraints,
+      deviceId: {
+        exact: cameraOptions.value
+      }
+    };
+
+    startStream(updatedConstraints);
+  };
+
+  play.onclick = () => {
+    if (streamStarted) {
+      video.play();
+      play.classList.add('d-none');
+      pause.classList.remove('d-none');
+      return;
+    }
+    const updatedConstraints = {
+      ...constraints,
+      deviceId: {
+        exact: cameraOptions.value
+      }
+    };
+    startStream(updatedConstraints);
+  };
+
+  const pauseStream = () => {
+    video.pause();
+    play.classList.remove('d-none');
+    pause.classList.add('d-none');
+  };
+
+  const doScreenshot = () => {
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    canvas.getContext('2d')?.drawImage(video, 0, 0);
+    screenshotImage.src = canvas.toDataURL('image/webp');
+    screenshotImage.classList.remove('d-none');
+  };
+
+  pause.onclick = pauseStream;
+  screenshot.onclick = doScreenshot;
+
+  const startStream = async (constraints: any) => {
+    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+    handleStream(stream);
+  };
+
+  const handleStream = (stream: MediaStream) => {
+    video.srcObject = stream;
+    play.classList.add('d-none');
+    pause.classList.remove('d-none');
+    screenshot.classList.remove('d-none');
+  };
+
+  const getCameraSelection = async () => {
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    const videoDevices = devices.filter(device => device.kind === 'videoinput');
+    const options = videoDevices.map(videoDevice => {
+      return `<option value="${videoDevice.deviceId}">${videoDevice.label}</option>`;
+    });
+    cameraOptions.innerHTML = options.join('');
+  };
+
+  getCameraSelection();
+}
 
 export default function Home() {
+  useEffect(() => {
+    onload();
+  }, []);
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      <div className="display-cover">
+        <video autoPlay></video>
+        <canvas className="d-none"></canvas>
+
+        <div className="video-options">
+          <select name="" id="" className="custom-select">
+            <option value="">Select camera</option>
+          </select>
+        </div>
+
+        <img className="screenshot-image d-none" alt="" />
+
+        <div className="controls">
+          <button className="btn btn-danger play" title="Play">
+            <i data-feather="play-circle"></i>
+          </button>
+          <button className="btn btn-info pause d-none" title="Pause">
+            <i data-feather="pause"></i>
+          </button>
+          <button className="btn btn-outline-success screenshot d-none" title="ScreenShot">
+            <i data-feather="image"></i>
+          </button>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
-  )
+  );
 }
